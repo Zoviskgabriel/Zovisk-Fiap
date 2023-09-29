a4e278f2107a3f10f45328382d5d6c8ed899733c
Gabriel Trimboli <gabrieltrimboli9@gmail.com>
2023-09-29 20:20:45 -0300

    salve

---
+++ Web-Development-Fiap/Aulas/Aula 4/variaveis.js
@@ -0,0 +1,11 @@
+// cria um varialvel com valor indefinido
+let nome;
+
+// cria uma variavel vazia 
+let disciplina = null;
+
+//cria uma varialvel com valor 
+let texto  = "Este e um exemplo";
+
+console.log(texto);
+
---
+++ Web-Development-Fiap/Checkpoint/script.js
@@ -0,0 +1,23 @@
+
+// Criar uma lista de variáveis, Listando cada variável em um console.log
+let nome = "Gabriel"
+let idade = 18
+let status = true
+let valorProduto = 10.50
+
+console.log(nome)
+console.log(idade)
+console.log(status)
+console.log(valorProduto)
+
+
+// Operações matemáticas com 4 variáveis, Mostrando os resultados no console.log
+let soma = 100 + 10 
+let subtração = 100 - 10
+let multiplicação = 100 * 10
+let divisão = 100 / 10
+
+console.log(soma)
+console.log(subtração)
+console.log(multiplicação)
+console.log(divisão)
