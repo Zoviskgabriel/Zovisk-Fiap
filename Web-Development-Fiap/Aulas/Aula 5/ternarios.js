0ef4b6e7a36739367deec3a1157ae0ba80d13d4f
Logon Aluno <logonrmlocal@fiap.com.br>
2023-10-06 11:44:39 -0300

    final da aula de web dia 06/10/2023

---
+++ Web-Development-Fiap/Aulas/Aula 5/ternarios.js
@@ -0,0 +1,12 @@
+let numero = 3;
+
+let resultado = (numero % 2 == 0) ? 'par': 'impar';
+
+if (valor > 7){
+    console.log('O valor é maior que sete!!!');
+}
+else {
+    console.log('O valor é outro')
+}
+
+
