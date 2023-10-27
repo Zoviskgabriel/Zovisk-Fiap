4b4d92a38bc8b70a04cfa0ad592b516d6c2c387e
Logon Aluno <logonrmlocal@fiap.com.br>
2023-10-27 10:26:44 -0300

    final da aula de front

---
+++ Web-Development-Fiap/Checkepoints/Checkpoint 2/animais.js
@@ -0,0 +1,9 @@
+const animals = ["Camaleão", "Tatu", "Guaxinin", "Panda", "Tamandua", "leão"];
+ 
+if (!(animals.includes("leão"))){
+    console.log("Leão não existe no array animals.")
+}
+ 
+else {
+    console.log("Existe leão no array animals.")
+}
